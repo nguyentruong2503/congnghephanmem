f0eb19eebac4d7418ba094538092e4224e272c0c
phu Nguyen <phu0908204@gmail.com>
2024-11-28 00:40:40 +0700

    mmm

--- MVC/Controllers/Danhsachtk.php
+++ MVC/Controllers/Danhsachtk.php
@@ -74,62 +74,7 @@ class Danhsachtk extends controller{
     //         }  
     //     }
 
-        //Xuất excel
-        // if(isset($_POST['btnExcel'])){
-        //     //code xuất excel
-        //     $objExcel=new PHPExcel();
-        //     $objExcel->setActiveSheetIndex(0);
-        //     $sheet=$objExcel->getActiveSheet()->setTitle('DSTK');
-        //     $rowCount=1;
-        //     //Tạo tiêu đề cho cột trong excel
-        //     $sheet->setCellValue('A'.$rowCount,'STT');
-        //     $sheet->setCellValue('B'.$rowCount,'Tên tên tài khoản');
-        //     $sheet->setCellValue('C'.$rowCount,'Mật khẩu');
-        //     $sheet->setCellValue('D'.$rowCount,'Loại Tài Khoản');
-            
         
-        //     //định dạng cột tiêu đề
-        //     $sheet->getColumnDimension('A')->setAutoSize(true);
-        //     $sheet->getColumnDimension('B')->setAutoSize(true);
-        //     $sheet->getColumnDimension('C')->setAutoSize(true);
-        //     $sheet->getColumnDimension('D')->setAutoSize(true);
-            
-        //     //gán màu nền
-        //     $sheet->getStyle('A1:D1')->getFill()->setFillType(\PHPExcel_Style_Fill::FILL_SOLID)->getStartColor()->setRGB('00FF00');
-        //     //căn giữa
-        //     $sheet->getStyle('A1:D1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-        //     //Điền dữ liệu vào các dòng. Dữ liệu lấy từ DB
-        //     $manguoidung=$_POST['txtManguoidung'];
-        //     $tentk=$_POST['txtTentk'];
-        //     $data=$this->ds->tk_find($manguoidung,$tentk);
-        //     $i=1;
-        //     while($row=mysqli_fetch_array($data)){
-        //         $rowCount++;
-        //         $sheet->setCellValue('A'.$rowCount,$i++);
-        //         $sheet->setCellValue('B'.$rowCount,$row['tentk']);
-        //         $sheet->setCellValue('C'.$rowCount,$row['mk']);
-        //         $sheet->setCellValue('D'.$rowCount,$row['loaitk']);
-        //     }
-        //     //Kẻ bảng 
-        //     $styleAray=array(
-        //         'borders'=>array(
-        //             'allborders'=>array(
-        //                 'style'=>PHPExcel_Style_Border::BORDER_THIN
-        //             )
-        //         )
-        //         );
-        //     $sheet->getStyle('A1:'.'D'.($rowCount))->applyFromArray($styleAray);
-        //     $objWriter=new PHPExcel_Writer_Excel2007($objExcel);
-        //     $fileName='ExportExcel_tkdanhsach.xlsx';
-        //     $objWriter->save($fileName);
-        //     header('Content-Disposition: attachment; filename="'.$fileName.'"');
-        //     header('Content-Type: application/vnd.openxlmformatsofficedocument.speadsheetml.sheet');
-        //     header('Content-Length: '.filesize($fileName));
-        //     header('Content-Transfer-Encoding:binary');
-        //     header('Cache-Control: must-revalidate');
-        //     header('Pragma: no-cache');
-        //     readfile($fileName);
-        //     }
     }
     //end xuat + tk
     //xoa
@@ -172,22 +117,7 @@ class Danhsachtk extends controller{
                 if($kq)
                     echo '<script>alert("Sửa thành công!");
                     window.location.href = "http://localhost/congnghephanmem/Danhsachtk";</script>';
-            //Kiểm tra trùng mã loại
-            // $kq1=$this->ds->check_trung($tentk);
-            // if($kq1){
-            //     echo '<script>alert("Trùng Tên Tài Khoản");
-            //     window.location.href = "http://localhost/congnghephanmem/Danhsachtk/";</script>';
-            // }
-            // else{
-            // //gọi hàm chèn dl loaisp_insert trong model tk_m
-            //     $kq=$this->ds->tk_update($manguoidung,$tentk,$mk,$loaitk);
-            //     if($kq)
-            //         echo '<script>alert("Sửa thành công!");
-            //         window.location.href = "http://localhost/congnghephanmem/Danhsachtk";</script>';
-            //     else
-            //         echo '<script>alert("Sửa thất bại!");
-            //         window.location.href = "http://localhost/congnghephanmem/Danhsachtk";</script>';
-            // }
+            
             }   
             //gọi lại giao diện
             $this->view('Masterlayout',[
